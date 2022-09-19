dd5282674f22d16de2d149f36a2a5ef3d1224310
Aaron <aaronbaut95@gmail.com>
2022-09-19 15:43:59 -0700

    Fix typos, add func to add books to array
    Fix typo for this.info function. Returns this.title
    
    Add items to array for testing

--- java.js
+++ java.js
@@ -6,10 +6,17 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     this.info = () => {
-        return `${this.book}` + `by` + `${this.author},` + `${this.pages},` + `` 
+        return `${this.title}` + ` is by ` + `${this.author},` + ` ${this.pages},` + `${this.read}`
     }
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(bookObj) {
+    return library.push(bookObj)
 }
+
+const harry = new Book('Harry', 'JK', '123', 'no')
+const bear = new Book('aa', 'bb', 'cc', 'dd')
+
+
+addBookToLibrary(harry);
+addBookToLibrary(bear);
