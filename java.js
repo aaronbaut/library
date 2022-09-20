c00564f91188c4c18f1745f643212f1b0d726dba
Aaron <aaronbaut95@gmail.com>
2022-09-19 19:40:37 -0700

    Display books on DOM with forEach method
    Use JSON.stringify to display objects as a string

--- java.js
+++ java.js
@@ -16,7 +16,20 @@ function addBookToLibrary(bookObj) {
 
 const harry = new Book('Harry', 'JK', '123', 'no')
 const bear = new Book('aa', 'bb', 'cc', 'dd')
+const goggins = new Book('Hurt', 'Goggins', '200', 'no')
 
 
 addBookToLibrary(harry);
 addBookToLibrary(bear);
+addBookToLibrary(goggins);
+
+const container = document.querySelector('.container')
+
+library.forEach(books => {
+    console.log(books)
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.textContent = JSON.stringify(books)
+    container.appendChild(card)
+    }
+)
